1628c0b2c94c576a7e2bc18886e83e1e217f8104
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-04-16 17:39:17 -0300

    Ajustes

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,19 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using cadProdutos.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace cadProdutos.Controllers;
 
-[ApiController]
 [Route("api/[controller]")]
+[ApiController]
 public class ProdutosController : ControllerBase{
 
-[HttpPost]
-public async Task<ActionResult<Produto>> cadastrar(Produto produtos)
-{
-    _context.TodoItems.Add(produtos);
-    await _context.SaveChangesAsync();
+    [HttpPost]
+    public async Task<ActionResult<Produto>> cadastrar(Produto produtos)
+    {
+        _context.Produto.Add(produtos);
+        await _context.SaveChangesAsync();
 
-    //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-    return CreatedAtAction(nameof(GetProduto), new { id = produtos.Id }, produtos);
-}
+        return CreatedAtAction(nameof(GetProduto), new { id = produtos.Id }, produtos);
+    }
+
+    
 }

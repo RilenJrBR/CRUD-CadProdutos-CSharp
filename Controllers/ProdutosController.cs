7a72922f25e164dac4c788e3732e431530d9c7d3
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-04-16 18:06:03 -0300

    implementando HttpGet

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -4,25 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace cadProdutos.Controllers;
 
+#region snippet_Route
 [Route("api/[controller]")]
 [ApiController]
-public class ProdutosController : ControllerBase{
+public class ProdutosController : ControllerBase
+#endregion
+{
 
     private readonly ProdutoContext _context;
 
-    public ProdutosController(ProdutoContext context)
-    {
+    public ProdutosController(ProdutoContext context){
         _context = context;
-    }
+    }    
 
     [HttpPost]
-    public async Task<ActionResult<Produto>> cadastrar(Produto produtos)
-    {
-        _context.Produto.Add(produtos);
+    public async Task<ActionResult<Produto>> cadastrar(Produto produtos){
+        _context.Produtos.Add(produtos);
         await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetProduto), new { id = produtos.Id }, produtos);
+        return CreatedAtAction(nameof(GetProdutos), new { id = produtos.Id }, produtos);
     }
 
-    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos(){
+        return await _context.Produtos.ToListAsync();
+    }
 }

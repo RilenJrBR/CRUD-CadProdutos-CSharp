95bfab6b60f012cf9e9e11c48c8f59305d415b6a
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-04-18 15:55:15 -0300

    implementando HttpPut

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -10,13 +10,18 @@ namespace cadProdutos.Controllers;
 public class ProdutosController : ControllerBase
 #endregion
 {
-
     private readonly ProdutoContext _context;
 
     public ProdutosController(ProdutoContext context){
         _context = context;
     }    
 
+    private bool ProdutoExists(long id)
+    {
+        return _context.Produtos.Any(e => e.Id == id);
+    }
+
+    
     [HttpPost]
     public async Task<ActionResult<Produto>> cadastrar(Produto produtos){
         _context.Produtos.Add(produtos);
@@ -24,9 +29,53 @@ public class ProdutosController : ControllerBase
 
         return CreatedAtAction(nameof(GetProdutos), new { id = produtos.Id }, produtos);
     }
+  
 
+    
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos(){
         return await _context.Produtos.ToListAsync();
     }
+    
+
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Produto>> GetProduto(long id){
+        var produtos = await _context.Produtos.FindAsync(id);
+
+        if (produtos == null){
+            return NotFound();
+        }
+
+        return produtos;
+    }
+    
+
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProduto(long id, Produto produtos){
+        if (id != produtos.Id){
+            return BadRequest();
+        }
+
+        _context.Entry(produtos).State = EntityState.Modified;
+
+        try{
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException){
+            if (!ProdutoExists(id))
+            {
+                return NotFound();
+            }
+            else{
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+   
+
+    
 }
